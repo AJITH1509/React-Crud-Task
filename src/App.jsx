c8ba875b5a3f1d92901be616c4d29dffcfe00230
AJITH1509 <ajithmuthu799@gmail.com>
2023-02-06 23:28:57 +0530

    Crud

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { EnrollForm } from "./Form";
+import { ButtonAppBar } from "./ButtonAppBar";
+
+function App() {
+  return (
+    <div>
+      <ButtonAppBar />
+      <EnrollForm />
+    </div>
+  );
+}
+
+export default App;
